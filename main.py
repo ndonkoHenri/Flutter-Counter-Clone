2cf9c302b665b50774d7a737854e4c74a093ca23
ndonkoHenri <robotcoder4@protonmail.com>
2023-02-07 22:08:42 +0100

    reorganised and refactored code | more comments

--- main.py
+++ main.py
@@ -2,41 +2,49 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    # the title of the app
     page.title = "Flet Counter App"
+
+    # a light/bright theme
     page.theme_mode = "light"
 
+    # use material 2 design theme | this is just to better mimic the Flutter example
+    page.theme = ft.Theme(use_material3=False)
+
+    # the page's alignment
     page.horizontal_alignment = "center"
     page.vertical_alignment = "center"
 
     def increment_counter(e):
-        """Increments the value of the counter_text object by 1"""
+        """Increment the value of the counter_text object by 1, and update the UI to reflect these changes."""
         counter_text.value = str(int(counter_text.value) + 1)
         page.update()
 
-    # the app's FAB
-    page.floating_action_button = ft.FloatingActionButton(
-        content=ft.Icon(ft.icons.ADD, color=ft.colors.WHITE), shape=ft.CircleBorder(),
-        on_click=increment_counter, tooltip="Increment", bgcolor=ft.colors.BLUE
-    )
-
     # the app's appbar
     page.appbar = ft.AppBar(
-        title=ft.Text("Flet Demo Home Page", color=ft.colors.WHITE),
-        bgcolor=ft.colors.BLUE, center_title=True
+        title=ft.Text("Flet Demo Home Page", color=ft.colors.WHITE),  # a title of white color
+        bgcolor=ft.colors.BLUE,  # a blue background color
+        center_title=True  # center the title || without this, the title will be on the left
     )
 
-    # contains the number to be incremented
-    counter_text = ft.Text("0", style=ft.TextThemeStyle.DISPLAY_LARGE)
+    # text that contains the counter number to be incremented
+    counter_text = ft.Text("0", style=ft.TextThemeStyle.DISPLAY_MEDIUM)
 
+    # the app's FAB
+    page.floating_action_button = ft.FloatingActionButton(
+        content=ft.Icon(ft.icons.ADD, color=ft.colors.WHITE),
+        shape=ft.CircleBorder(),  # gives the button a round/circle shape
+        on_click=increment_counter,  # the callback to be executed when this button is clicked
+        tooltip="Increment",  # the text to be shown when this button is hovered
+        bgcolor=ft.colors.BLUE  # a blue background color
+    )
+
+    # adding our widgets/controls to the page/UI
     page.add(
-        ft.Column(
-            controls=[
-                ft.Text("You have pushed the button this many times:"),
-                counter_text
-            ],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER
-        )
+        ft.Text("You have pushed the button this many times:"),
+        counter_text
     )
 
 
+# open a browser tab containing the app | remove the view parameter to open in a native OS window
 ft.app(target=main, view=ft.WEB_BROWSER)
